a333f3c2d5e2f124822450b822111a3929d08957
vivekkumar1994 <viveksnh933@gmail.com>
2025-03-07 15:40:48 +0530

    latest

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
         <Auth0Provider
-        domain="dev-hon88oyxqdfrljz4.us.auth0.com"
-        clientId="Rru6SctIEDMStNBOOTUBGGYbH5MjMMeu"
-        authorizationParams={{
-          redirect_uri: 'https://melodious-beignet-2955ad.netlify.app/'
+         domain="dev-hon88oyxqdfrljz4.us.auth0.com"
+         clientId="Rru6SctIEDMStNBOOTUBGGYbH5MjMMeu"
+         authorizationParams={{
+           redirect_uri: window.location.origin
         }}
       >
             <ThemeProvider theme={theme}>
